c3e1764bab304cd4d31c2672f31cc130a59236f1
James Timms <59345798+Timmsy1998@users.noreply.github.com>
2023-11-03 14:32:15 +0000

    Added Validation Rules

--- backend/app/Http/Controllers/JobController.php
+++ backend/app/Http/Controllers/JobController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\Rule;
+use Validator;
 use Illuminate\Http\Request;
 use App\Models\Job;
 
 class JobController extends Controller
 {
+    protected function validateJobRequest($request)
+    {
+        return Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'salary' => 'required|string|max:255',
+            'company' => 'required|string|max:255',
+            'postedAt' => 'required|date',
+        ]);
+    }
+
     public function index()
     {
         return Job::all();
@@ -14,6 +27,12 @@ class JobController extends Controller
 
     public function store(Request $request)
     {
+        $validator = $this->validateJobRequest($request);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400); // Bad Request
+        }
+
         $job = Job::create($request->all());
         return response()->json($job, 201);
     }
@@ -25,6 +44,12 @@ class JobController extends Controller
 
     public function update(Request $request, Job $job)
     {
+        $validator = $this->validateJobRequest($request);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400); // Bad Request
+        }
+
         $job->update($request->all());
         return response()->json($job, 200);
     }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('jobs', JobController::class);
+Route::resource('jobs', JobController::class)->only(['index', 'store', 'show', 'update', 'destroy']);

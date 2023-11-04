78fc8f1b686f7a219a23731586e0b1ba38fc4ae4
James Timms <59345798+Timmsy1998@users.noreply.github.com>
2023-11-04 14:05:46 +0000

    built config with options I may use for this project

---
+++ frontend/src/config/config.js
@@ -0,0 +1,8 @@
+const config = {
+    APP_NAME: 'Job Posting Application',
+    DEV_MODE: true,
+    ITEMS_PER_PAGE: 10,
+    JOBS_API: 'http://127.0.0.1:8000/api/jobs',
+  };
+  
+  export default config;
